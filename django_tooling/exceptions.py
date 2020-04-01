6a453099aac83e441aba4d3f2b23f177e055f6c7
Tim Mundt <dev@tim-erwin.de>
2020-04-01 15:44:42 +0200

    pass kwargs through ValidationError to parent class

--- django_tooling/exceptions.py
+++ django_tooling/exceptions.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError as BaseValidationError
 
 
 class ValidationError(BaseValidationError):
-    def __init__(self, message, fieldName=None):
+    def __init__(self, message, fieldName=None, **kwargs):
         if not fieldName:
             fieldName = "general"
-        super().__init__({fieldName: message})
+        super().__init__({fieldName: message}, **kwargs)
